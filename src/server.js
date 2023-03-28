0cac95e7133870de524aa679503ea86401414160
raissaputra <raissaputra02@gmail.com>
2023-03-28 13:17:37 +0700

    post and get note

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
-console.log('belajar backend');
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server sedang berjalan pada ${server.info.uri}`);
+};
+
+init();
